e3c53c6111ed20849888216ccc314a5cf733d8e1
monsst <monsst@stud.ntnu.no>
2022-05-04 16:48:46 +0200

    tried adding color change to bookmarks

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,16 @@ function showForm() {
         document.getElementById('newUserForm').style.display = "block";
         formStatus = "shown";
         console.log("function was executed with if");
-    }else {
+    } else {
         document.getElementById('newUserForm').style.display = "none";
         formStatus = "shown";
         console.log("function was executed with else");
     }
+}
+
+function bookmarkColorChange() {
+    const bookmark = document.getElementsByClassName("fa-bookmark");
+    bookmark.addEventListener('click', function onClick() {
+        bookmark.style.backgroundColor = 'red';
+    });
 }
